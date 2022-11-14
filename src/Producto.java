5829b88e1e148c45667097edfc14c2e6b7f49301
santino fazio <115169486+SantinoFazio@users.noreply.github.com>
2022-11-14 11:46:57 -0300

    add: class Main and Productos

---
+++ src/Producto.java
@@ -0,0 +1,59 @@
+public class Producto {
+
+    double weight;
+    double length;
+    double profundity;
+    double tall;
+    double typeProduct;
+
+    public Producto(double peso, double largo, double profundida, double alto, double typeProduct) {
+        this.weight = weight;
+        this.length = length;
+        this.profundity = profundity;
+        this.tall = tall;
+        this.typeProduct = typeProduct;
+    }
+
+
+    public double getPeso() {
+        return weight;
+    }
+
+    public void setPeso(double peso) {
+        this.weight = peso;
+    }
+
+    public double getLargo() {
+        return length;
+    }
+
+    public void setLargo(double largo) {
+        this.length = largo;
+    }
+
+    public double getProfundity() {
+        return profundity;
+    }
+
+    public void setProfundity(double profundity) {
+        this.profundity = profundity;
+    }
+
+    public double getAlto() {
+        return tall;
+    }
+
+    public void setAlto(double alto) {
+        this.tall = alto;
+    }
+    public double getTypeProduct() {
+        return typeProduct;
+    }
+
+    public void setTypeProduct(double typeProduct) {
+        this.typeProduct = typeProduct;
+    }
+
+
+
+}
